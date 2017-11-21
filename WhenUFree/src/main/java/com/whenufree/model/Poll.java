c22053697e52c192ed1a99a8834b282de1b94ad2
wmaanxg <maxwizdomer@gmail.com>
2017-11-21 13:28:03 -0500

    revised poll

--- WhenUFree/src/main/java/com/whenufree/model/Poll.java
+++ WhenUFree/src/main/java/com/whenufree/model/Poll.java
@@ -4,9 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Column;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.JoinColumn;
 import javax.persistence.FetchType;
+
+import java.util.Set;
+import java.util.HashSet;
 
 import com.whenufree.model.FriendGroup;
 
@@ -15,7 +18,12 @@ import com.whenufree.model.FriendGroup;
 public class Poll{
     private Long pollId;
     private String description;
-    
+
+    private Set<PollOption> options;
+
+    public Poll(){
+	this.options = new HashSet<>();
+    }
     
     /**
      * Gets the value of pollId
@@ -52,8 +60,19 @@ public class Poll{
      *
      * @param argDescription Value to assign to this.description
      */
-    public void setDescription(final String argDescription) {
+    public void setDescription(String argDescription) {
 	this.description = argDescription;
     }
+
+    @OneToMany(mappedBy = "poll", fetch = FetchType.LAZY)
+    public Set<PollOption> getOptions(){
+	return this.options;
+    }
+
+    public void setOptions(Set<PollOption> options){
+	this.options = options;
+    }
+
+    
 
 }
