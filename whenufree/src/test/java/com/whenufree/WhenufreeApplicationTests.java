64621f9bd0a40015cbdaa288f92a9edd17b8dc14
wmaanxg <maxwizdomer@gmail.com>
2017-11-22 19:27:16 -0500

    added testing specific settings

--- whenufree/src/test/java/com/whenufree/WhenufreeApplicationTests.java
+++ whenufree/src/test/java/com/whenufree/WhenufreeApplicationTests.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.TestPropertySource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:application.properties")
 public class WhenufreeApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
