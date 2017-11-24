ba6be46b86c1d9db1896dc852960949f816c9ec3
Junjie Wang <junjie2412@gmail.com>
2017-11-24 18:36:23 -0500

    changing  my version of the app so that nothing is hurt

--- WhenUFree/src/main/java/com/whenufree/WhenufreeApplication.java
+++ WhenUFree/src/main/java/com/whenufree/WhenufreeApplication.java
@@ -6,13 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WhenufreeApplication {
 
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		SpringApplication.run(WhenufreeApplication.class, args);
 	}
-=======
-    public static void main(String[] args) {
-	SpringApplication.run(WhenufreeApplication.class, args);
-    }
->>>>>>> 78cc519364ce84a24e404c29a15819d0438042fa
 }
