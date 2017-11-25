ce740259413540e3f07ef4d29fe4f5402ecc9124
wmaanxg <maxwizdomer@gmail.com>
2017-11-25 13:12:29 -0500

    added basic corsfilter, changed testing configuration

--- whenufree/src/main/java/com/whenufree/config/CORSConfiguration.java
+++ whenufree/src/main/java/com/whenufree/config/CORSConfiguration.java
@@ -3,6 +3,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CORSConfiguration{
@@ -11,9 +12,9 @@ public class CORSConfiguration{
     public WebMvcConfigurer corsConfigurer(){
 	return new WebMvcConfigurerAdapter(){
 	    @Override
-	    public void addCorsMappins(CorsRegistry registry){
+	    public void addCorsMappings(CorsRegistry registry){
 		registry.addMapping("/**");
 	    }
-	}
+	};
     }
 }
