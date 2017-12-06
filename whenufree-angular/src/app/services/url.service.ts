d25b510856b78a68797dd65887861e8c39908bd6
wmaanxg <maxwizdomer@gmail.com>
2017-12-06 01:59:54 -0500

    changed to work with aws

--- whenufree-angular/src/app/services/url.service.ts
+++ whenufree-angular/src/app/services/url.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UrlService {
 
-    url: string = 'http://localhost:8080'
+    url: string = 'http://ec2-18-216-0-70.us-east-2.compute.amazonaws.com:8080'
     constructor() { }
 
     getUrl(){
--- whenufree-angular/src/app/services/user.service.ts
+++ whenufree-angular/src/app/services/user.service.ts
@@ -3,28 +3,29 @@ import { User } from '../domain/user'
 import { Observable } from 'rxjs/Observable'
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from '../message.service';
-
+import {UrlService} from './url.service';
 
 @Injectable()
 export class UserService {
-	userUrl = 'http://localhost:8080/user';
+	
 	headers = new HttpHeaders()
 		.set('X-Requested-With', 'XMLHttpRequest'); // to suppress 401 browser popup
 	options = {headers: this.headers, withCredentials: true};
 
 
-	constructor( private http: HttpClient,
-		     private messageService: MessageService) { }
+    constructor( private url: UrlService,
+		 private http: HttpClient,
+		 private messageService: MessageService) { }
 
 	getUser(): Observable<User> {
-		return this.http.get<User>(this.userUrl, this.options)
+	    return this.http.get<User>(this.url.getUrl() + '/user', this.options)
 	}
 
 	updateUser( user: User): Observable<User> {
-		return this.http.post<User>('http://localhost:8080//updateuser', user, this.options);
+	    return this.http.post<User>(this.url.getUrl() + '/updateuser', user, this.options);
 	}
 
 	deleteUser( user: User): Observable<User> {
-		return this.http.post<User>(this.userUrl, user, this.options);
+		return this.http.post<User>(this.url.getUrl() + '/user', user, this.options);
 	}
 }
